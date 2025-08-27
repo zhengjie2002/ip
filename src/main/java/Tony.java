630f17b1e03ff4fda9d00605c031467df41f41f7
zhengjie2002 <tan.zhengjie6@gmail.com>
2025-08-27 10:47:46 +0800

    Add feature to repeat user input

--- src/main/java/Tony.java
+++ src/main/java/Tony.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Tony {
     public static void main(String[] args) {
+        String userInput = "";
+        Scanner in = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Tony");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+
+        while (!userInput.equals("bye")) {
+            userInput = in.nextLine();
+            System.out.println("____________________________________________________________");
+            System.out.println(userInput);
+            System.out.println("____________________________________________________________");
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
