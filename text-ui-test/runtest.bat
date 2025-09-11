f5c31ab8ae84f044b52a5621cef0d7968f23dc22
zhengjie2002 <tan.zhengjie6@gmail.com>
2025-09-11 17:30:30 +0800

    Update automatic testing with package path

--- text-ui-test/runtest.bat
+++ text-ui-test/runtest.bat
@@ -7,7 +7,7 @@ REM delete output from previous run
 if exist ACTUAL.TXT del ACTUAL.TXT
 
 REM compile the code into the bin folder
-javac  -cp ..\src\main\java -Xlint:none -d ..\bin ..\src\main\java\*.java
+javac  -cp ..\src\main\java\tony -Xlint:none -d ..\bin ..\src\main\java\tony\command\*.java ..\src\main\java\tony\exceptions\*.java ..\src\main\java\tony\task\*.java ..\src\main\java\tony\ui\*.java
 IF ERRORLEVEL 1 (
     echo ********** BUILD FAILURE **********
     exit /b 1
