ce5d7a1f90d2cd571c6d2d3cdbca43ac0777a2d7
ZhengJie <zhengjie@192.168.0.139>
2025-09-28 13:13:09 +0800

    Refactor format as static variable

--- src/main/java/tony/task/Deadline.java
+++ src/main/java/tony/task/Deadline.java
@@ -5,6 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class Deadline extends Task {
     protected LocalDate deadlineDate;
+    private static final String OUTPUT_DATE_FORMAT = "MMM d yyyy";
 
     public Deadline(String description, LocalDate deadlineDate) {
         super(description);
@@ -18,6 +19,6 @@ public class Deadline extends Task {
     @Override
     public String toString() {
         return ("[D]" + (isDone ? "[X] " : "[ ] ") + description + " (by: " +
-                deadlineDate.format(DateTimeFormatter.ofPattern("MMM d yyyy")) + ")");
+                deadlineDate.format(DateTimeFormatter.ofPattern(OUTPUT_DATE_FORMAT)) + ")");
     }
 }
